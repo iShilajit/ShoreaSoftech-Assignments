48758bbf1c8a2a72632e0622d7276fd38af48965
Shilajit <shilajit2014paul@gmail.com>
2022-11-08 18:12:19 +0530

    done

--- cart/src/App.js
+++ cart/src/App.js
@@ -2,13 +2,27 @@
 import './App.css';
 import Cart from './Components/Cart';
 import Product from './Components/Product';
-
+import React, { useState } from 'react'
 function App() {
+  const Products=[
+    {id: 1, name:`Product-1`,price:100},
+    {id: 2, name:`Product-2`,price:200},
+    {id: 3, name:`Product-3`,price:300},
+
+]
+
+
+const[price,setPrice]=useState(0)
+const[price2,setPrice2]=useState(0)
+const[price3,setPrice3]=useState(0)
+
+
+console.log(price,price2,price3);
   return (
     <div className="App">
       <div className="main">
-          <Product/>
-      <Cart/>
+          <Product Products={Products} price={price} setPrice={setPrice} price2={price2} setPrice2={setPrice2}  price3={price3} setPrice3={setPrice3}/>
+      <Cart Products={Products} price={price} setPrice={setPrice} price2={price2} setPrice2={setPrice2}  price3={price3} setPrice3={setPrice3}/>
           </div>
      
     </div>
